72a00e8c21d4d5a4f4ad7758a90578ce260fad64
Giovanni Marangi <giovanni.marangi@studio.unibo.it>
2020-11-03 20:25:07 +0100

    Test script finished. Now all class methods, except for plotting one, have a test function

--- Tests/DCM_test.py
+++ Tests/DCM_test.py
@@ -10,24 +10,123 @@ Created on Sun Oct 18 20:33:32 2020
 
 from DCM.DCM import DCM
 import numpy as np
+import pytest 
 
 
 
-DCM_test_results=[]
+#Testing new_species function
+def test_new_species():
 
-
-
-#Run New_species function
-Eco_system_test=DCM()
-Eco_system_test.New_species()
-species=Eco_system_test.Livings()[0][1]
-
-if (Eco_system_test.N[species]!=0):
-    DCM_test_results.append(True)
-else:
-    DCM_test_results.append(False)
-############################################################################################
+    Eco_system_test=DCM()
+    Eco_system_test.new_species()
     
+    assert Eco_system_test.N.any(), "DCM.new_species() method failed"
+
+
+############################################################################################
+#Testing get_parameters()/set_parameters(arr) 
+def test_get_set_params_DCM():
+    
+    Eco_system_test=DCM()
+    assert Eco_system_test.get_params()==Eco_system_test.default_params, "DCM.get_params() method failed"
+    test_params=range(1,9)
+    Eco_system_test.set_params(test_params)
+    
+    assert (Eco_system_test.get_params()==np.array(test_params)).all(), "set_params() method failed"
+
+############################################################################################
+#Testing livings(pop) method
+def test_livings():
+    Eco_system_test=DCM()
+    
+    Eco_system_test.new_species()
+    
+    assert (Eco_system_test.livings()[0]==np.where(Eco_system_test.N!=0)[0]).all(), "DCM.livings() method failed"
+    test_pop=Eco_system_test.N
+    #Recreating an empty ecosystem
+    Eco_system_test=DCM()
+    assert (Eco_system_test.livings(pop=test_pop)[0]==np.where(test_pop!=0)[0]).all(),"DCM.livings(pop=test_pop) method failed"
+
+
+############################################################################################
+#testing animals() method
+def test_animals():
+    Eco_system_test=DCM()
+    
+    Eco_system_test.new_species()
+    assert Eco_system_test.animals()==np.setdiff1d(Eco_system_test.livings(),0),"DCM.animals() method failed"
+############################################################################################
+#testing get_interaction() method
+def test_get_interaction():
+    
+    Eco_system_test=DCM()
+    Eco_system_test.new_species()
+    species=Eco_system_test.livings()[0][1]
+    
+    new_species_preys=np.where(Eco_system_test.gamma[species]>0)
+    new_species_predators=np.where(Eco_system_test.gamma[species]<0)
+    
+    #predators list is empty since there is only one predator in the ecosystem
+    assert Eco_system_test.get_interaction(species)[0]==new_species_preys,"DCM.get_interaction() method failed"
+
+############################################################################################
+#testing extinction() method
+def test_extinction():
+    
+    Eco_system_test=DCM()
+    Eco_system_test.new_species()
+    species=Eco_system_test.livings()[0][1]
+    
+    Eco_system_test.N[species]=Eco_system_test.N_c-1e-6   #associo all'unica specie vivente un energia minore del minimo N_c
+    
+    Eco_system_test.extinction()
+    
+    assert Eco_system_test.N[species]==0,"DCM.extinction() method failed to remove the extincted biomass"
+    assert not (Eco_system_test.gamma[species]).any(),"DCM.extinction() method failed to remove interactions from gamma"
+
+
+############################################################################################
+#testing f() method
+def test_f():
+    
+    Eco_system_test=DCM()
+    starting_energy=np.copy(Eco_system_test.N[0])
+    Eco_system_test.evolve(T=5,I=0,dt=1)
+    assert starting_energy<Eco_system_test.N[0],"DCM.f() failed to provide the correct dN/dt to scipy.odeint"
+    
+    Eco_system_test.new_species()
+    species=Eco_system_test.livings()[0][1]
+    starting_energy=np.copy(Eco_system_test.N[species])
+    #Added a new animal species and then let basal goes extincted, so that once the system evolves without migration flux, we expect the new species to goes extincted as well
+    
+    Eco_system_test.N[0]=Eco_system_test.N_c-1e-6
+    Eco_system_test.extinction()
+    Eco_system_test.evolve(T=20,I=0,dt=1)
+    
+    assert Eco_system_test.N[species]<starting_energy,"DCM.f() failed to provide the correct dN/dt to scipy.odeint"
+    
+
+############################################################################################
+#testing migration() method
+def test_migration():
+    
+
+    Eco_system_test=DCM()
+    
+    
+    Eco_system_test.migration(I=5)
+    
+    assert len(Eco_system_test.livings()[0]==6),"DCM.migration(I) method failed"
+    
+
+
+
+
+
+
+
+
+"""    
 #Build an only-Basal resources ecosystem to test the Lotka-Volterra equation
 Eco_system_test=DCM()
 start_biomass=Eco_system_test.N[0]
@@ -38,6 +137,7 @@ if (start_biomass<Eco_system_test.N[0]):
     DCM_test_results.append(True)
 else:
     DCM_test_results.append(False)
+
 ############################################################################################
 
 
@@ -72,3 +172,5 @@ if(err_idx.size==0):
     print("Test succedeed!")
 else:
     print("Test failed with these follow index:",err_idx)
+    
+"""
--- Tests/IBM_test.py
+++ Tests/IBM_test.py
@@ -8,147 +8,249 @@ Created on Sat Oct 10 18:54:04 2020
 
 from IBM.IBM import IBM
 import numpy as np
+import pytest
 
-IBM_test_passed=[]
+Area=1000
+RM_method="RM_test"
+CM_method="CM_test"
 
-#A=Animal species, B=Basal species, -> = paired with
+def test_build_interactions():
+    Eco_system_test=IBM(Area,RM_method)
+    for rows in Eco_system_test.C[Eco_system_test.Basals[-1]+1:]:
+        assert rows.any(),"Found a species with no interactions assigned: IBM.build_interaction() for RM method failed"
+            
+    Eco_system_test=IBM(Area,CM_method)
+    for species in Eco_system_test.Predators:
+        assert Eco_system_test.C[species].any()," Found a species with no interactions assigned: IBM.build_interaction() for CM method failed"
+        
+    
+############################################################################################
+#Testing get_parameters()/set_parameters(arr) 
+        
+def test_get_set_params_IBM():
+    
+    Eco_system_test=IBM(Area,RM_method)
+    assert Eco_system_test.get_params()==Eco_system_test.default_par, "IBM.get_params() method failed"
+    test_params=range(1,9)
+    Eco_system_test.set_params(test_params)
+    
+    assert (Eco_system_test.get_params()==np.array(test_params)).all(), "set_params() method failed"
 
-#err_idx=[0]
+############################################################################################
+#testing add_individual()
+    
+def test_add_individual():
+    Eco_system_test=IBM(Area,RM_method)
+    Eco_system_test.add_individual(Species=10,energy=100)
+    
+    assert Eco_system_test.species_alive().any()," IBM.add_individual() failed to add a new species to the ecosystem"
+    assert Eco_system_test.Individuals[0][0] == 100, "IBM.add_individual() failed to give starting energy to new species"
+    
+    
+def test_deaths():
+    Eco_system_test=IBM(Area,RM_method)
+    Eco_system_test.add_individual()
+    
+    
+    Eco_system_test.Individuals[0][0]=0
+    Eco_system_test.deaths()
+    assert Eco_system_test.Individuals.size==0,"IBM.deaths() failed to remove 0 energy individuals"
+    
+
+############################################################################################
+#Testing basals_counts()
+    
+def test_basals_counts():
+    methods=[RM_method,CM_method]
+    
+    for method in methods:
+        Eco_system_test=IBM(Area,method)
+        spec1=Eco_system_test.Basals[1]
+        spec2=Eco_system_test.Basals[2]
+        
+        Eco_system_test.add_individual(Species=spec1)
+        Eco_system_test.add_individual(Species=spec2)
+        
+        assert Eco_system_test.basals_counts()==2,"IBM.basals_count failed to returns the correct number of living basals"
+     
+############################################################################################
+#Testing births()  
+        
+def test_births():
+    Eco_system_test=IBM(Area,RM_method)
+    #testing for predator species
+    spec=Eco_system_test.Predators[0]
+    Eco_system_test.add_individual(Species=spec)
+    Eco_system_test.Individuals[0][0]=Eco_system_test.E_rep+1e-1
+    
+    Eco_system_test.births()
+    
+    assert Eco_system_test.Individuals[:,1].size==2, " IBM.births() failed to add new individual from species with Energy>E_rep"
+    assert Eco_system_test.species_alive().size==1, "IBM.births() failed to add a new individual belonging to the parent's same species"
+    
+    #testing for basals species (Area limitation)
+    Area_test=1
+    Eco_system_test=IBM(Area_test,RM_method)
+    spec=Eco_system_test.Basals[1]
+    Eco_system_test.add_individual(Species=spec,energy=Eco_system_test.E_rep+1e-1)
+    
+    Eco_system_test.births()
+    #aborting birth due the full capacity of the ecosystem
+    assert Eco_system_test.Individuals[:,1].size==1,"IBM.births() failed to abort births, when ecosystem Area is satured."
+
+############################################################################################
+#testing create_pairs()
+    
+def test_create_pairs():
+    Eco_system_test=IBM(Area,RM_method)
+    #adding 7 individuals
+    for new_species in range(0,7):
+        Eco_system_test.add_individual()
+        
+    pairs=Eco_system_test.create_pairs()
+    
+    assert np.shape(pairs)==(3,2),"IBM.create_pairs() failed"
+    #check if any pair has duplicate item 
+    for pair in pairs:
+        assert len(set(list(pair)))==len(pair),"IBM.create_pairs() failed to create pairs of distinct individuals"
+        
+############################################################################################
+#testing get_individual(ID)
+        
+def test_get_individual():
+    Eco_system_test=IBM(Area,RM_method)
+    Eco_system_test.add_individual(Species=Eco_system_test.Predators[1],energy=50)
+    new_species_ID=Eco_system_test.Individuals[0][2]
+    
+    assert (Eco_system_test.get_individual(ID=new_species_ID)==Eco_system_test.Individuals[0]).all(),"IBM.get_individual(ID) failed to returns info array about specified individual"
+    
+    species_alive=Eco_system_test.species_alive()[0][0]
+    
+    not_living_species=species_alive+1
+    #test if when get_individual is feed with not existing species, return False
+    assert not Eco_system_test.get_individual(ID=not_living_species),"IBM.get_individual() failed to return False in not-existing species case"
+    
+############################################################################################
+#testing all the interaction dynamics cases
+    
+#A=Animal species, B=Basal species, -> = paired with      
+def test_interaction_dynamics():
+        
 #A1 -> A2 ---- No interaction
-Eco_system_test=IBM(100,"RM_test")
-A1=Eco_system_test.Basals[-1]+1
+    
+    Eco_system_test=IBM(Area,RM_method)
+    A1=Eco_system_test.Basals[-1]+1
+    
+    predators_list=Eco_system_test.get_predators(A1)
+    preys_list=Eco_system_test.get_preys(A1)
+    basal_list=Eco_system_test.Basals
+    
+    A1_interactions=np.append(predators_list,preys_list)
+    A1_interactions=np.append(A1_interactions,basal_list)
+    A1_no_interact=np.setdiff1d(Eco_system_test.all_species,A1_interactions)
+    
+    A2=A1_no_interact[0]
+    
+    Eco_system_test.add_individual(Species=A1)
+    Eco_system_test.add_individual(Species=A2)
+    starting_energy=Eco_system_test.Individuals[0][0]
+    Eco_system_test.evolve(Time=1,I=1,dt=2,verbose=False)
+    
+    A1_energy=Eco_system_test.get_individual(0)[0]
+    A2_energy=Eco_system_test.get_individual(1)[0]
+    
+    assert (A1_energy<starting_energy and A2_energy<starting_energy),"IBM.interaction_dynamics fail with A-A no interaction dynamics"
 
-predators_list=Eco_system_test.get_predators(A1)
-preys_list=Eco_system_test.get_preys(A1)
-basal_list=Eco_system_test.Basals
-
-A1_interactions=np.append(predators_list,preys_list)
-A1_interactions=np.append(A1_interactions,basal_list)
-A1_no_interact=np.setdiff1d(Eco_system_test.all_species,A1_interactions)
-
-A2=A1_no_interact[0]
-
-Eco_system_test.Add_individual(Species=A1)
-Eco_system_test.Add_individual(Species=A2)
-starting_energy=Eco_system_test.Individuals[0][0]
-Eco_system_test.Evolve(Time=1,I=1,dt=2,verbose=False)
-
-A1_energy=Eco_system_test.Get_individual(0)[0]
-A2_energy=Eco_system_test.Get_individual(1)[0]
-
-if(A1_energy<starting_energy and A2_energy<starting_energy):
-    IBM_test_passed.append(True)
 ###################################################################
-#err_idx=[1]
 #B1 -> B2 ----- no interaction
     
-Eco_system_test=IBM(100,"RM_test")
-
-Eco_system_test.Add_individual(Species=0)
-Eco_system_test.Add_individual(Species=1)
-
-Eco_system_test.Evolve(Time=1,I=1,dt=2,verbose=False)
-B1_energy=Eco_system_test.Get_individual(0)[0]
-B2_energy=Eco_system_test.Get_individual(1)[0]
-
-if (B1_energy>starting_energy and B2_energy>starting_energy):
-    IBM_test_passed.append(True)
-else:
-    IBM_test_passed.append(False)
-
-
+    Eco_system_test=IBM(Area,RM_method)
+    
+    Eco_system_test.add_individual(Species=0)
+    Eco_system_test.add_individual(Species=1)
+    
+    Eco_system_test.evolve(Time=1,I=1,dt=2,verbose=False)
+    B1_energy=Eco_system_test.get_individual(0)[0]
+    B2_energy=Eco_system_test.get_individual(1)[0]
+    
+    assert (B1_energy>starting_energy and B2_energy>starting_energy),"IBM.interaction_dynamics fail with B-B no interact dynamics"
+    
 ###################################################################    
-#err_idx=[2]
 #A1 -> A2 ----- A1 predate A2
     
-Eco_system_test=IBM(100,"RM_test")
-A1=Eco_system_test.Basals[-1]+1
+    Eco_system_test=IBM(Area,RM_method)
+    A1=Eco_system_test.Basals[-1]+1
+    
+    A1_preys=Eco_system_test.get_preys(A1)
+    A1_preys=np.setdiff1d(A1_preys,Eco_system_test.Basals)
+    
+    A2=A1_preys[0]
+    
+    Eco_system_test.add_individual(Species=A1)
+    Eco_system_test.add_individual(Species=A2)
+    starting_energy=Eco_system_test.Individuals[0][0]
+    Eco_system_test.evolve(Time=1,I=1,dt=2,verbose=False)
+    
+    A1_energy=Eco_system_test.get_individual(0)[0]
+    
+    assert (Eco_system_test.N_deaths>0),"IBM.interaction_dynamics failed with A_pred-A_prey interact dynamics"
 
-A1_preys=Eco_system_test.get_preys(A1)
-A1_preys=np.setdiff1d(A1_preys,Eco_system_test.Basals)
-
-A2=A1_preys[0]
-
-Eco_system_test.Add_individual(Species=A1)
-Eco_system_test.Add_individual(Species=A2)
-starting_energy=Eco_system_test.Individuals[0][0]
-Eco_system_test.Evolve(Time=1,I=1,dt=2,verbose=False)
-
-A1_energy=Eco_system_test.Get_individual(0)[0]
-
-if(Eco_system_test.N_deaths>0):
-    IBM_test_passed.append(True)
-else:
-    IBM_test_passed.append(False)
 ###################################################################    
-#err_idx=[3]
 #A1->B1 ----- No interaction, Basals_energy>=E_rep
     
-Eco_system_test=IBM(100,"RM_test")
-A1=Eco_system_test.Basals[-1]+1
-A1_preys=Eco_system_test.get_preys(A1)
-
-A1_no_interact=np.setdiff1d(Eco_system_test.Basals,A1_preys)
-B1=A1_no_interact[0]
-
-Eco_system_test.Add_individual(Species=A1)
-Eco_system_test.Add_individual(Species=B1,energy=200)
-
-Eco_system_test.Evolve(Time=1,I=1,dt=2,verbose=False)
-
-A1_energy=Eco_system_test.Get_individual(0)[0]
-
-if (A1_energy<starting_energy and Eco_system_test.N_births==1):
-    IBM_test_passed.append(True)
-else:
-    IBM_test_passed.append(False)
+    Eco_system_test=IBM(Area,RM_method)
+    A1=Eco_system_test.Basals[-1]+1
+    A1_preys=Eco_system_test.get_preys(A1)
+    
+    A1_no_interact=np.setdiff1d(Eco_system_test.Basals,A1_preys)
+    B1=A1_no_interact[0]
+    
+    Eco_system_test.add_individual(Species=A1)
+    Eco_system_test.add_individual(Species=B1,energy=200)
+    
+    Eco_system_test.evolve(Time=1,I=1,dt=2,verbose=False)
+    
+    A1_energy=Eco_system_test.get_individual(0)[0]
+    
+    assert (A1_energy<starting_energy and Eco_system_test.N_births==1),"IBM.interaction_dynamics faild with A-B no interaction dynamics"
+    
+    
 ###################################################################    
-#err_idx[4]
 #A1->B1 ------- No interaction , Basal_energy>=E_rep but Basals_counts>=Area
     
-Eco_system_test=IBM(area=1,method="RM_test")
-A1=Eco_system_test.Basals[-1]+1
-A1_preys=Eco_system_test.get_preys(A1)
+    Eco_system_test=IBM(area=1,method=RM_method)
+    A1=Eco_system_test.Basals[-1]+1
+    A1_preys=Eco_system_test.get_preys(A1)
+    
+    A1_no_interact=np.setdiff1d(Eco_system_test.Basals,A1_preys)
+    B1=A1_no_interact[0]
+    
+    Eco_system_test.add_individual(Species=A1)
+    Eco_system_test.add_individual(Species=B1,energy=200)
+    Eco_system_test.evolve(Time=1,I=1,dt=2,verbose=False)
+    
+    A1_energy=Eco_system_test.get_individual(0)[0]
+    
+    assert (A1_energy<starting_energy and Eco_system_test.N_births==0),"IBM.interaction_dynamics failed with A-B No interact, Basal_energy>=E_rep but Basals_counts>=Area"
 
-A1_no_interact=np.setdiff1d(Eco_system_test.Basals,A1_preys)
-B1=A1_no_interact[0]
-
-Eco_system_test.Add_individual(Species=A1)
-Eco_system_test.Add_individual(Species=B1,energy=200)
-Eco_system_test.Evolve(Time=1,I=1,dt=2,verbose=False)
-
-A1_energy=Eco_system_test.Get_individual(0)[0]
-
-if (A1_energy<starting_energy and Eco_system_test.N_births==0):
-    IBM_test_passed.append(True)
-else:
-    IBM_test_passed.append(False)
 ###################################################################    
-#err_idx[5]
 #A1->B1 ----- A1 predate B1
-Eco_system_test=IBM(100,method="RM_test")
-B1=1
-B1_predators=Eco_system_test.get_predators(1)
-A1=B1_predators[0][0]
+    
+    Eco_system_test=IBM(Area,RM_method)
+    B1=1
+    B1_predators=Eco_system_test.get_predators(1)
+    A1=B1_predators[0][0]
+    
+    Eco_system_test.add_individual(Species=A1)
+    Eco_system_test.add_individual(Species=B1,energy=150)
+    Eco_system_test.evolve(Time=1,I=1,dt=2,verbose=False)
+    
+    A1_energy=Eco_system_test.get_individual(0)[0]
+    
+    assert (A1_energy>starting_energy and Eco_system_test.N_deaths>0),"IBM.interaction_dynamics failed with A_pred-B_prey dynamic"
 
-Eco_system_test.Add_individual(Species=A1)
-Eco_system_test.Add_individual(Species=B1,energy=150)
-Eco_system_test.Evolve(Time=1,I=1,dt=2,verbose=False)
-
-A1_energy=Eco_system_test.Get_individual(0)[0]
-
-if(A1_energy>starting_energy and Eco_system_test.N_deaths>0):
-    IBM_test_passed.append(True)
-else:
-    IBM_test_passed.append(False)
-####################################################################
-IBM_test_passed=np.array(IBM_test_passed)
-err_idx=IBM_test_passed[IBM_test_passed==False]
-if(err_idx.size==0):
-    print("Test succedeed!")
-else:
-    print("Test failed with these follow index:",err_idx)
-
+        
+    
 
 
 
