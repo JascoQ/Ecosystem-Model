5b893d33e0127aa5ff3d96498442e1b7c87e62ac
Giovanni Marangi <giovanni.marangi@studio.unibo.it>
2020-11-16 19:04:06 +0100

    Updated to 1.0 version, now the classes are stable and tested

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ project_name = "Ecosystem_Simulation-JascoQ"
 
 setup(
     name = project_name,
-    version = "0.0.9",
+    version = "1.0",
     author = "Giovanni Marangi",
     author_email = "giovanni.marangi@studio.unibo.it",
     description = ("Two different approaches to simulate an ecosystem"),
